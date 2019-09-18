27eda1dfadb1e07101d711c7d7e5a90a81fea425
Nstats <17801238715@163.com>
2019-09-18 15:03:59 +0800

    09.18

---
+++ run_bert_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+python run_bert.py \
+--model_type bert \
+--model_name_or_path chinese_RoBERTa_zh_Large_pytorch \
+--do_train \
+--do_eval \
+--do_test \
+--data_dir ./data/data_0 \
+--output_dir ./output_test/fold_0 \
+--save_steps 50 \
+--max_seq_length 10 \
+--split_num 1 \
+--lstm_hidden_size 512 \
+--lstm_layers 1 \
+--lstm_dropout 0.1 \
+--eval_steps 200 \
+--per_gpu_train_batch_size 2 \
+--gradient_accumulation_steps 2 \
+--warmup_steps 0 \
+--per_gpu_eval_batch_size 32 \
+--learning_rate 5e-5 \
+--adam_epsilon 1e-6 \
+--weight_decay 0 \
+--train_steps 200
