e6bdd2ac54018ffebe856c6c22eb18dedafcd958
ubuntu <ubuntu@localhost.localdomain>
2019-10-04 13:34:38 +0800

    10.05

--- run_bert.sh
+++ run_bert.sh
@@ -11,22 +11,22 @@ python run_bert.py \
 --do_eval \
 --do_test \
 --data_dir ./data/data_$i \
---output_dir ./out_RoBERTa_large_3epo_3split_64bs_0.2dout/fold_$i \
---classifier 'guoday' \
+--output_dir ./out_RoBERTa_large_3epo_1split_128bs_MLP/fold_$i \
+--classifier 'MLP' \
 --max_seq_length 512 \
---split_num 3 \
+--split_num 1 \
 --lstm_hidden_size 512 \
 --lstm_layers 1 \
---lstm_dropout 0.2 \
+--lstm_dropout 0.1 \
 --eval_steps 200 \
---per_gpu_train_batch_size 64 \
+--per_gpu_train_batch_size 128 \
 --gradient_accumulation_steps 32 \
 --warmup_steps 0 \
 --per_gpu_eval_batch_size 32 \
 --learning_rate 5e-5 \
 --adam_epsilon 1e-6 \
 --weight_decay 0 \
---train_steps 9000
+--train_steps 4500
 
 done
 
